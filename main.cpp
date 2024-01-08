f3e340c77fd1333ab524337d0d3ddded71f41700
Ghandilyan21 <138888382+Ghandilyan21@users.noreply.github.com>
2024-01-08 21:43:44 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,26 +3,27 @@ int main(int argc, char const *argv[])
 {
     Vector<int> vec {1,2,3,4,5,6,7,8,9};
     Vector<int> vec1 {234,52,4,12,8};
-    Stack<int> st(vec);
-    Stack<int> st1(vec1);
+    Stack<int> st1(vec);
+    Stack<int> st2 {std::move(st1)};
+    //Stack<int> st1(vec1);
+    
+    while (!st2.empty())
+    {
+        std::cout << st2.top() << std::endl;
+        st2.pop();
+    }
+    while (!st1.empty())
+    {
+        std::cout << st1.top() << std::endl;
+        st1.pop();
+    }
     
     // while (!st.empty())
     // {
     //     std::cout << st.top() << std::endl;
+    //     st.pop();
         
     // }
-    // while (!st1.empty())
-    // {
-    //     std::cout << st1.top() << std::endl;
-        
-    // }
-    st = st1;
-    while (!st.empty())
-    {
-        std::cout << st.top() << std::endl;
-        st.pop();
-        
-    }
     // st.push(3);
     // st.push(89);
     // st.push(4);
